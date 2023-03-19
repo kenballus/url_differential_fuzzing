eb490cdd62d58a12386287d52101b06f70c21a9b
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2023-03-19 12:10:46 -0400

    update config

--- config.py
+++ config.py
@@ -5,10 +5,13 @@
 #############################################################################################
 
 from pathlib import PosixPath
-from typing import NamedTuple, List, Dict
+from typing import NamedTuple, List, Dict, Optional
 import os
 
 # The directory where the seed inputs are
+# The seeds are the files from which all the fuzzing inputs are produced,
+# so it's important that the seeds are a decently representative sample
+# of the inputs accepted by the targets.
 SEED_DIR: PosixPath = PosixPath("./seeds")
 
 # Where program traces end up
@@ -17,12 +20,16 @@ TRACE_DIR: PosixPath = PosixPath("./traces")
 # Time in milliseconds given to each process
 TIMEOUT_TIME: int = 100000
 
+# Set this to false if you only care about exit status differentials
+# (i.e. the programs you're testing aren't expected to have identical output on stdout)
+OUTPUT_DIFFERENTIALS_MATTER: bool = True
+
 # Roughly how many processes to allow in a generation (within a factor of 2)
 ROUGH_DESIRED_QUEUE_LEN: int = 1000
 
-# The location of the installed AFL, if it's not in the PATH.
-# Note that python-afl will always use the installation in your PATH.
-AFL_ROOT: PosixPath = PosixPath("/home/bkallus/fuzzing/AFLplusplus/")
+# AFL++ and AFL differ a little about what goes on stdout and what goes on stderr.
+# Set this flag if you're using AFL++ so that can be handled correctly.
+USES_AFLPLUSPLUS: bool = True
 
 
 class TargetConfig(NamedTuple):
