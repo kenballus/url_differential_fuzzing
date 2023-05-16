2af883ae4cd8f66f72a736e27cb34a2b166f451e
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2023-05-16 08:31:35 -0400

    libwget base64

--- targets/libwget/libwget_target.c
+++ targets/libwget/libwget_target.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <string.h>
 #include <wget/wget.h>
 
 #define MAX_URL_LEN (32768)
@@ -6,29 +7,34 @@ static char url_string[MAX_URL_LEN];
 
 int main(void) {
     fgets(url_string, MAX_URL_LEN, stdin);
-    wget_iri const *const parsed_url = wget_iri_parse(url_string, "utf-8");
+    wget_iri *const parsed_url = wget_iri_parse(url_string, "utf-8");
     if (parsed_url == NULL) {
         return 1;
     }
 
     wget_iri_scheme scheme = parsed_url->scheme;
-    char const *const userinfo = parsed_url->userinfo;
-    char const *const password = parsed_url->password;
-    char const *const host = parsed_url->host;
-    uint16_t const port = parsed_url->port;
-    char const *const path = parsed_url->path;
-    char const *const query = parsed_url->query;
-    char const *const fragment = parsed_url->fragment;
+    char *const userinfo = wget_base64_encode_alloc(parsed_url->userinfo, parsed_url->userinfo != NULL ? strlen(parsed_url->userinfo) : 0);
+    char *const password = wget_base64_encode_alloc(parsed_url->password, parsed_url->password != NULL ? strlen(parsed_url->password) : 0);
+    char *const host = wget_base64_encode_alloc(parsed_url->host, parsed_url->host != NULL ? strlen(parsed_url->host) : 0);
+    char port_str[6];
+    sprintf(port_str, "%d", parsed_url->port);
+    char *const port = wget_base64_encode_alloc(port_str, strlen(port_str));
+    char *const path = wget_base64_encode_alloc(parsed_url->path, parsed_url->path != NULL ? strlen(parsed_url->path) : 0);
+    char *const query = wget_base64_encode_alloc(parsed_url->query, parsed_url->query != NULL ? strlen(parsed_url->query) : 0);
+    char *const fragment = wget_base64_encode_alloc(parsed_url->fragment, parsed_url->fragment != NULL ? strlen(parsed_url->fragment) : 0);
 
-    printf("Scheme:   %s\n", scheme == 0 ? "http" : "https");
-    printf("Userinfo: %s", userinfo);
+    printf("{\"scheme\":\"%s\",\"userinfo\":\"%s\"", scheme == 0 ? "http" : "https", userinfo);
     if (password != NULL) {
         printf(":%s", password);
     }
-    puts("");
-    printf("Host:     %s\n", host);
-    printf("Port:     %d\n", port);
-    printf("Path:     %s\n", path);
-    printf("Query:    %s\n", query);
-    printf("Fragment: %s\n", fragment);
+    printf(",\"host\":\"%s\",\"port\":\"%s\",\"path\":\"%s\",\"query\":\"%s\",\"fragment\":\"%s\"}\n", host, port, path, query, fragment);
+
+    free(parsed_url);
+    free(userinfo);
+    free(password);
+    free(host);
+    free(port);
+    free(path);
+    free(query);
+    free(fragment);
 }
