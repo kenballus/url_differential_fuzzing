8b743082bb8c77e0694e91f12654a1ef1e9c6729
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2023-05-18 16:12:39 -0400

    update installer and make boost delete old installs

--- targets/boost_url/get_boost.sh
+++ targets/boost_url/get_boost.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # There is no need to build boost with afl-clang because we only use the header-only portions of the library.
-git clone --recurse-submodules "https://github.com/boostorg/boost.git" && cd boost && cmake . && make -j$(nproc)
+rm -rf boost && git clone --recurse-submodules "https://github.com/boostorg/boost.git" && cd boost && cmake . && make -j$(nproc)
