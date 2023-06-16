9f033e52503bc9b60189bf864dce333de9c9dd2b
JohnJamesUtley <john.j.utley.24@dartmouth.edu>
2023-06-16 09:58:21 -0400

    Cleans up analysis scripts

--- benchmarking/parserOverlap.py
+++ benchmarking/parserOverlap.py
@@ -19,6 +19,8 @@ os.chdir(working_dir)
 
 def main():
 
+    # Ensure that the necessary files are found
+
     run_of_interest_name = sys.argv[1]
 
     if run_of_interest_name not in os.listdir("runs/"):
@@ -31,9 +33,13 @@ def main():
 
     differential_folder = os.path.join(run_of_interest_path, "differentials")
 
+    # Create dictionary to track shared bugs
+
     diff_count: dict[tuple[int, int], int] = {}
     for idx_pair in itertools.combinations(range(len(TARGET_CONFIGS)), 2):
         diff_count[idx_pair] = 0
+
+    # Try each differential and add if overlap
 
     for differential_file_name in tqdm(os.listdir(differential_folder)):
         with open(os.path.join(differential_folder, differential_file_name), "rb") as differential_file:
@@ -43,6 +49,8 @@ def main():
             for idx_pair in diff_count.keys():
                 if statuses[idx_pair[0]] != statuses[idx_pair[1]] or not all(compare_parse_trees(parse_trees[idx_pair[0]], parse_trees[idx_pair[1]])):
                     diff_count[idx_pair] += 1
+
+    # Print Results
 
     for idx_pair, tc_pair in zip(diff_count.keys(), itertools.combinations((tc.name for tc in TARGET_CONFIGS), 2)):
         print(f"{tc_pair} -----> {diff_count[idx_pair]}")
