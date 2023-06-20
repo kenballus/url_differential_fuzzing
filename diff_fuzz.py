98b4a7f9a557e78c1cb85c31e995564b2685a887
JohnJamesUtley <jjutley231@gmail.com>
2023-06-20 15:50:23 -0400

    Removes use of .format

--- diff_fuzz.py
+++ diff_fuzz.py
@@ -465,8 +465,7 @@ if __name__ == "__main__":
         with open(_result_file_path, "wb") as _result_file:
             _result_file.write(_final_differential)
             print(
-                "Differential: {0:20} Path: {1}".format(str(_final_differential), str(_result_file_path)),
-                file=sys.stderr,
+                f"Differential: {str(_final_differential):20} Path: {str(_result_file_path)}", file=sys.stderr
             )
 
     print('{"differentials":\n    [')
