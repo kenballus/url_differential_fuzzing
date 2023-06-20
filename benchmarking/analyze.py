329a070861b8712343ca63732e3732891328c97b
JohnJamesUtley <jjutley231@gmail.com>
2023-06-20 12:11:14 -0400

    Makes analyses use uuid for file naems

--- benchmarking/analyze.py
+++ benchmarking/analyze.py
@@ -3,6 +3,7 @@ import sys
 import json
 import shutil
 import itertools
+import uuid
 from pathlib import PosixPath
 
 import matplotlib.pyplot as plt  # type: ignore
@@ -23,13 +24,13 @@ os.chdir(working_dir)
 
 
 # Check that necessary files exist for the given run
-def assert_data(uuid: str):
+def assert_data(run_uuid: str):
     if not os.path.isdir(PosixPath(REPORT_DIR)):
         raise FileNotFoundError("Report Directory doesn't exist!")
-    if not os.path.isfile(PosixPath(REPORT_DIR).joinpath(f"{uuid}_report.json")):
-        raise FileNotFoundError(f"{uuid} doesn't have a report file!")
-    if not os.path.isdir(PosixPath(RESULTS_DIR).joinpath(uuid)):
-        raise FileNotFoundError(f"{uuid} doesn't have a differentials folder!")
+    if not os.path.isfile(PosixPath(REPORT_DIR).joinpath(f"{run_uuid}_report.json")):
+        raise FileNotFoundError(f"{run_uuid} doesn't have a report file!")
+    if not os.path.isdir(PosixPath(RESULTS_DIR).joinpath(run_uuid)):
+        raise FileNotFoundError(f"{run_uuid} doesn't have a differentials folder!")
 
 
 # Plot a run onto a given axis
@@ -49,9 +50,11 @@ def plot_data(run_name: str, report_file_path: PosixPath, axis: np.ndarray):
         times.append(float(differential["time"]))
     # Plot Things
     axis[0].plot(np.array(times), np.array(count), label=run_name)
-    axis[0].set_title("Bugs vs Time")
+    axis[0].set_xlabel("Time (s)")
+    axis[0].set_ylabel("Bugs")
     axis[1].plot(np.array(generations), np.array(count))
-    axis[1].set_title("Bugs vs Generation")
+    axis[1].set_xlabel("Generations")
+    axis[1].set_ylabel("Bugs")
 
 
 def get_fingerprint_differentials(
@@ -84,12 +87,15 @@ def get_fingerprint_differentials(
 
 
 # Given dictionaries of fingerprints in each run and the bytes those fingerprints correspond to
-def summarize_common_bugs(runs_to_analyze: set[tuple[str, str]], analysis_file_path: PosixPath):
+def summarize_common_bugs(
+    runs_to_analyze: set[tuple[str, str]], analysis_file_path: PosixPath, analysis_name: str
+):
     run_differentials: dict[str, dict[fingerprint_t, bytes]] = {}
-    for run_name, uuid in runs_to_analyze:
-        run_differentials[run_name] = get_fingerprint_differentials(PosixPath(RESULTS_DIR).joinpath(uuid))
-    # Clear current analysis file
-    open(analysis_file_path, "wb").close()  # Clears File
+    for run_name, run_uuid in runs_to_analyze:
+        run_differentials[run_name] = get_fingerprint_differentials(PosixPath(RESULTS_DIR).joinpath(run_uuid))
+    # Setup analysis file
+    with open(analysis_file_path, "wb") as analysis_file:
+        analysis_file.write(f"Analysis: {analysis_name}\n".encode("utf-8"))
     # Get list of combos from big to small
     combos = list(
         list(run for (run, _), enabled in zip(runs_to_analyze, enables) if enabled)
@@ -122,20 +128,24 @@ def summarize_common_bugs(runs_to_analyze: set[tuple[str, str]], analysis_file_p
 
 
 def build_relative_analysis(analysis_name: str, runs_to_analyze: set[tuple[str, str]]):
-    figure, axis = plt.subplots(2)
-    figure.tight_layout(h_pad=2)
+    figure, axis = plt.subplots(2, 1, constrained_layout=True)
+    figure.suptitle(analysis_name, fontsize=16)
 
-    for run_name, uuid in runs_to_analyze:
+    for run_name, run_uuid in runs_to_analyze:
         print(f"Analyzing: {run_name}")
-        assert_data(uuid)
-        plot_data(run_name, PosixPath(REPORT_DIR).joinpath(f"{uuid}_report.json"), axis)
+        assert_data(run_uuid)
+        plot_data(run_name, PosixPath(REPORT_DIR).joinpath(f"{run_uuid}_report.json"), axis)
+
+    analysis_uuid: str = str(uuid.uuid4())
+    analysis_folder: PosixPath = PosixPath(ANALYSES_DIR).joinpath(analysis_uuid)
+    os.mkdir(analysis_folder)
+    analysis_file_path: PosixPath = PosixPath(ANALYSES_DIR).joinpath(analysis_uuid).joinpath(analysis_uuid)
 
     figure.legend(loc="upper left")
-    analysis_file_path: PosixPath = PosixPath(ANALYSES_DIR).joinpath(analysis_name)
     plt.savefig(analysis_file_path.with_suffix(".png"), format="png")
     plt.close()
 
-    summarize_common_bugs(runs_to_analyze, analysis_file_path.with_suffix(".txt"))
+    summarize_common_bugs(runs_to_analyze, analysis_file_path.with_suffix(".txt"), analysis_name)
 
 
 def main():
